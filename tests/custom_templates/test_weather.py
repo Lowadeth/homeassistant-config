751a2f8cf081ac77940393d7f56d446b6f3aaebf
Peter Zorn <peter-zorn@peter-zorn.de>
2025-06-10 19:45:38 +0200

    days_until_no_rain

--- tests/custom_templates/test_weather.py
+++ tests/custom_templates/test_weather.py
@@ -124,4 +124,45 @@ def test_hours_until_rain_macro():
         forecast_data = rain_data['weather.openweathermap']['forecast']
         result_rain_data = json.loads(template.module.hours_until_rain(forecast_data).strip())
         assert result_rain_data['time'] == '2025-06-10T20:00:00+00:00'
-        assert result_rain_data['resolution'] == 'hours' 
+        assert result_rain_data['resolution'] == 'hours'
+
+def test_days_until_no_rain_macro():
+    """Test the days_until_no_rain macro."""
+    # Setup Jinja2 environment with whitespace control
+    env = Environment(
+        loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), '../../usr/share/hassio/homeassistant')),
+        trim_blocks=True,
+        lstrip_blocks=True
+    )
+    
+    # Get the template
+    template = env.get_template('custom_templates/weather.jinja')
+    
+    # Test with rain in forecast
+    test_data_with_rain = [
+        {'datetime': '2025-06-10T11:00:00+00:00', 'precipitation': 8.66},
+        {'datetime': '2025-06-11T11:00:00+00:00', 'precipitation': 4.0},
+        {'datetime': '2025-06-12T11:00:00+00:00', 'precipitation': 0}
+    ]
+    result_with_rain = json.loads(template.module.days_until_no_rain(test_data_with_rain).strip())
+    assert result_with_rain['time'] == '2025-06-12T11:00:00+00:00'
+    assert result_with_rain['resolution'] == 'days'
+
+    # Test with always rain in forecast
+    test_data_always_rain = [
+        {'datetime': '2025-06-10T11:00:00+00:00', 'precipitation': 8.66},
+        {'datetime': '2025-06-11T11:00:00+00:00', 'precipitation': 4.0},
+        {'datetime': '2025-06-12T11:00:00+00:00', 'precipitation': 2.0}
+    ]
+    result_always_rain = json.loads(template.module.days_until_no_rain(test_data_always_rain).strip())
+    assert result_always_rain['time'] == 'always_rain'
+    assert result_always_rain['resolution'] == 'days'
+
+    # Test with it-stops-raining-in-days.yaml data
+    with open(os.path.join(os.path.dirname(__file__), 'it-stops-raining-in-days.yaml'), 'r') as file:
+        rain_data = yaml.safe_load(file)
+        # Extract forecast data from the YAML file
+        forecast_data = rain_data['weather.openweathermap']['forecast']
+        result_rain_data = json.loads(template.module.days_until_no_rain(forecast_data).strip())
+        assert result_rain_data['time'] == '2025-06-12T11:00:00+00:00'
+        assert result_rain_data['resolution'] == 'days' 
